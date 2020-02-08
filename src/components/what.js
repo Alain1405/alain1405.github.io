7145858ad364b26a92eb3516f6f411f5ce5c0f54
Alain Scialoja <ascialoja@gmail.com>
2020-02-08 16:14:42 +0900

    First version

---
+++ src/components/what.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react"
+
+class What extends Component {
+  constructor(props) {
+    super(props)
+    this.doings = [
+      "job",
+      "passion",
+      "craft",
+      "hobby",
+      "desire",
+      "occupation",
+      "vocation",
+    ]
+    this.delay = 1500
+    this.state = { doing: "job", idx: 0 }
+  }
+  changeDoing() {
+    this.setState({
+      idx: this.state.idx + 1,
+    })
+    if (this.state.idx >= this.doings.length) {
+      this.setState({
+        idx: 0,
+      })
+    }
+    return this.doings[this.state.idx]
+  }
+  render() {
+    return <span>{this.state.doing}</span>
+  }
+  componentDidMount() {
+    this.interval = setInterval(
+      () =>
+        this.setState({
+          doing: this.changeDoing(),
+        }),
+      this.delay
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+}
+
+export default What
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,40 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import MeImage from "../components/me_image"
+import What from "../components/what"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="Alain Scialoja" />
+    <div className="col-md-6">
+      <h1>Hi people</h1>
+      <p>
+        Building tech is my <What></What>
+      </p>
+      <h2>Currently</h2>
+      <h3>
+        VP of Engineering @{" "}
+        <a href="https://reustle.co/">Reustle LLC</a>
+      </h3>
+      <p>Building great software</p>
+      <h2>Of note</h2>
+      <h3>
+        VP of Engineering @ {" "}
+        <a href="https://infarm.com">Infarm</a> - Berlin
+      </h3>
+      <p>Living the urban farming revolution</p>
+      <h3>
+        CTO @ {" "}
+        <a href="https://www.bumbeelabs.com/">BumbeeLabs</a> - Stockholn
+      </h3>
+      <p>Pioneering footfall analytics</p>
+
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="col-md-6">
+      <MeImage/>
+    </div>
   </Layout>
 )
 
