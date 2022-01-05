9d18beba62cc33a6cc8372d5a7bbce39d79bf87b
Alain Scialoja <>
2022-01-05 14:58:28 +0800

    Improved Bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,48 +4,102 @@ import Layout from "../components/layout"
 import MeImage from "../components/me_image"
 import What from "../components/what"
 import Seo from "../components/seo"
+import { Grid } from 'theme-ui'
+import Helmet from "react-helmet"
 
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Alain Scialoja" />
-    <div className="col-md-6">
-      <h1>Alain Scialoja</h1>
-      <p className="headline">
-        Building <strong>technology</strong> is my <What></What>.
-      </p>
-      <h2>Currently</h2>
-      <h3>
-        VP of Engineering @ <a href="https://reustle.co/">Reustle LLC</a> -
-        Remote
-      </h3>
-      <p>
-        Building great software, globally. Ensuring the success of multiple,
-        multifaceted software projects. Providing agile and tech leadership
-        training to clients.
-      </p>
-      <h2>Of note</h2>
-      <h3>
-        VP of Engineering @ <a href="https://infarm.com">Infarm</a> - Berlin
-      </h3>
-      <p>
-        I designed and directed the development of Infarm's Urban Farming
-        platform, building a lean and successful team of multidisciplinary
-        talents from scratch, across hardware and software, leading to a
-        successful series A funding.
-      </p>
-      <h3>
-        CTO @ <a href="https://www.bumbeelabs.com/">BumbeeLabs</a> - Stockholm
-      </h3>
-      <p>
-        Pioneered footfall analytics and user flow acquisition and processing in
-        outdoor environments.
-      </p>
-    </div>
-    <div className="col-md-6">
-      <MeImage />
-    </div>
+    <Seo title="Alain Scialoja - Bio" />
+        <p className="headline">
+          Building <strong>technology</strong> is my <What></What>.
+        </p>
+    <Grid gap={2} columns={[1, null, 2]}>
+      <div>
+        {/* <h2>Expertise and domains of interest</h2> */}
+        <p>CTO, Technical Product Owner, Technical Lead for IoT, e-learning (Open edX), Blockchain and sustainability.</p>
+        <h2>Currently</h2>
+        <h3>
+          VP of Engineering @ <a href="https://strata.co.jp/" target="_blank">Strata KK</a> -
+          Remote
+        </h3>
+        <p>
+          Leading the development of multiple international projects across a variety of domains. Establishing and managing remote teams of experts. Providing agile and tech leadership
+          training to clients.
+        </p>
+        <h2>Of note</h2>
+        <h3>
+          VP of Engineering @ <a href="https://infarm.com" target="_blank">Infarm</a> - Berlin
+        </h3>
+        <p>
+          I designed and directed the development of Infarm's Urban Farming
+          platform, building a lean and successful team of multidisciplinary
+          talents from scratch, across hardware and software, leading to a
+          successful series A funding.
+        </p>
+        <h3>
+          CTO @ <a href="https://www.bumbeelabs.com/" target="_blank">BumbeeLabs</a> - Stockholm
+        </h3>
+        <p>
+          Pioneered footfall analytics and user flow acquisition and processing in
+          outdoor environments.
+        </p>
+      </div>
+      <div>
+        <MeImage />
+      </div>
+    </Grid>
+    <Helmet>
+      <script src="https://formspree.io/js/formbutton-v1.min.js"></script>
+
+      <script>
+        {`
+              window.formbutton = window.formbutton || function() {
+                (formbutton.q = formbutton.q || []).push(arguments)
+              };
+              
+              formbutton("create", {
+                  action: "https://formspree.io/f/mayopjgb",
+                  title: "Contact Alain",
+                  fields: [
+                      {
+                          type: "text",
+                          label: "Full name:",
+                          name: "name",
+                          required: true,
+                          placeholder: ""
+                      },{
+                          type: "email",
+                          label: "Email:",
+                          name: "email",
+                          required: true,
+                          placeholder: "your@email.com"
+                      },
+                      {
+                          type: "textarea",
+                          label: "Message:",
+                          name: "message",
+                          placeholder: "",
+                      },
+                      {
+                          type: "submit"
+                      }
+                  ],
+                  styles: {
+                      title: {
+                          backgroundColor: "gray"
+                      },
+                      button: {
+                          backgroundColor: "gray"
+                      }
+                  }
+              });
+              `}
+      </script>
+    </Helmet>
+
   </Layout>
+
 )
 
 export default IndexPage
